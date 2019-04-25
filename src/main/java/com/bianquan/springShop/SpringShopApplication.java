0e48cf2c7973e7f912f29edebd2fa014bb34702a
ZhuYunlong <920200256@qq.com>
2019-04-25 17:43:59 +0800

    second commit

--- src/main/java/com/bianquan/springShop/SpringShopApplication.java
+++ src/main/java/com/bianquan/springShop/SpringShopApplication.java
@@ -1,9 +1,11 @@
 package com.bianquan.springShop;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@EnableAutoConfiguration
+@SpringBootApplication(scanBasePackages = "com.bianquan")
 public class SpringShopApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/bianquan/springShop/demo/HelloSpringBoot.java
+++ src/main/java/com/bianquan/springShop/demo/HelloSpringBoot.java
@@ -1,13 +1,16 @@
 package com.bianquan.springShop.demo;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.bianquan.springShop.common.utils.R;
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api("测试用HELLO")
 @RestController
 public class HelloSpringBoot {
-    @RequestMapping(path = {"/helloSpringBoot"})
-    public String HelloSpring (){
+    @GetMapping("/helloSpringBoot")
+    public R HelloSpring (){
         System.out.println("hello spring boot");
-        return "hello spring boot";
+        return R.ok("haole");
     }
 }
